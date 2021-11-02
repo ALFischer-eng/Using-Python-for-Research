7a1b8c4fa80986cb1e1400b44133a375dd762ad6
ALFischer-eng <93222883+ALFischer-eng@users.noreply.github.com>
2021-11-02 09:30:36 -0700

    Update Bird migration case study.py

--- Bird Migration Case Study/Bird migration case study.py
+++ Bird Migration Case Study/Bird migration case study.py
@@ -3,14 +3,23 @@
 Created on Thu Oct 28 10:20:23 2021
 
 @author: Ali
+
+This code uses data from a gps bird-tracking study of three different 
+seagulls. We will make several plots describing their migration speeds 
+and patterns. 
+PLEASE NOTE: The last plot of this code required the cartopy module
+which can be found here: https://scitools.org.uk/cartopy/docs/latest/
 """
-"""Import bird data from csv"""
+
+
+"""Import bird data from csv, may need to edit for your pwd"""
 import pandas as pd
 
 birddata = pd.read_csv("bird_tracking.csv")
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 """Start by isolating the data for one bird, Eric, and plotting his longitude and latitude as an xy plot"""
 ix = birddata.bird_name == "Eric"
@@ -19,6 +28,8 @@ x,y = birddata.longitude[ix], birddata.latitude[ix]
 
 plt.figure(figsize = (7,7))
 plt.plot(x,y,".")
+plt.show()
+
 
 """ Now inlcude all of the birds' data and make a new version of that plot"""
 bird_names = pd.unique(birddata.bird_name)
@@ -30,12 +41,15 @@ for bird_name in bird_names:
     x,y = birddata.longitude[ix], birddata.latitude[ix]
     plt.plot(x,y,".", label = bird_name)
 
-
+plt.show()
 
 plt.plot(x,y,".")
 plt.xlabel("longitude")
-plt.ylabel("Longitude")
+plt.ylabel("Latitude")
 plt.legend(loc = "lower right")
+plt.show()
+
+
 
 """Now let's look at just Eric's average speed, and demonstrate two different ways to deal with NaNs in the data"""
 ix = birddata.bird_name == "Eric"
@@ -45,19 +59,22 @@ speed = birddata.speed_2d[ix]
 ind = np.isnan(speed)
 
 """Now plot the cleaned data as a histogram of Eric's speeds during the data collection period"""
+
 plt.figure(figsize = (8,4))
 speed = birddata.speed_2d[birddata.bird_name == "Eric"]
 ind = np.isnan(speed)
-plt.hist(speed[~ind], bins =np.linspace(0.30,20), normed =True)
+plt.hist(speed[~ind], bins =np.linspace(0.30,20))
 plt.xlabel("2D speed (m/s)")
 plt.ylabel("Frequency")
 
-"""plotting the same data using pandas, which will deal with NaNs for you"""
+"""plotting using pandas, which will deal with NaNs for you"""
 
 birddata.speed_2d.plot(kind="hist", range = [0,30])
 plt.xlabel("2d speed")
 
+
 """Now let's figure out Eric's average daily speed, but first we need to convert the date/time information to the datetime format"""
+
 import datetime
 
 date_str = birddata.date_time[0]
@@ -71,21 +88,19 @@ for k in range(len(birddata)):
         
 birddata["timestamp"]= pd.Series(timestamps, index = birddata.index)
 
+
 """Create an elapsed time column that will indicate the number of days since the start of data collection"""
 times = birddata.timestamp[birddata.bird_name =="Eric"]
 elapsed_time = [time-times[0] for time in times]
 
 elapsed_time[1000]/datetime.timedelta(days = 1)
- 
-"""Plot Eric's speed by elapsed day"""
-plt.plot(np.array(elapsed_time)/datetime.timedelta(days=1))
-plt.xlabel("Observation")
-plt.ylabel("Elapsed time (days)")
+
+
 
 """ Now let's figure out Eric's daily mean speed and plot it as mean speed by day since the start of observation"""
 data = birddata[birddata.bird_name == "Eric"]
 time = data.timestamp
-elapsed_time = [time -times[0] for time in times]
+
 elapsed_days = np.array(elapsed_time)/datetime.timedelta(days = 1)
 
 
@@ -105,6 +120,7 @@ plt.figure(figsize = (8,6))
 plt.plot(daily_mean_speed)
 plt.xlabel("Day")
 plt.ylabel("Mean Speed (m/s)")
+plt.show()
 
 
 """Now we're going to use cartopy to make a mercator projected map of the flight paths of all three birds"""
@@ -129,5 +145,5 @@ for name in bird_names:
     ax.plot(x,y,".", transform = ccrs.Geodetic(),label = name)
 
 plt.legend(loc= "upper left")
-
+plt.show()
 
